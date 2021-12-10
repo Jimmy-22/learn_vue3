1372b8a3df60f9c59785d384a823f99cf1ed3a02
ferrytsui <ferrytsui@163.com>
2021-12-11 01:04:46 +0800

    init project;todolist

---
+++ src/components/Todolist.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 @click="add">{{count}}</h1>
+  </div>  
+</template>
+
+<script setup>
+import {ref} from "vue"
+let count = ref(2)
+
+function add() {
+  count.value++
+}
+</script>
+
+<style>
+
+</style>
