cc04730e28b8e084b1f7dbde8253d390fc14a5bf
ferrytsui <ferrytsui@163.com>
2021-12-14 00:17:32 +0800

    初步完成五星组件

---
+++ src/pages/RateDemo.vue
@@ -0,0 +1,13 @@
+<template>
+<Rate :value="1" />
+<Rate :value="4" theme='black'/>
+<Rate :value="3" theme='blue'/>
+</template> 
+
+<script setup>
+import { ref } from 'vue';
+import Rate from '../components/Rate.vue'
+
+let score = ref(1)
+
+</script>
